6382f0311765d4bd7a23f7f68507cfa7ab24e053
Colby Harrison <colbyharrison@Colbys-Mac-mini.home>
2022-01-05 10:28:29 -0800

    update dependencies and pubkey

--- programs/anchor-escrow/src/lib.rs
+++ programs/anchor-escrow/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Hfd7V12kj9AENQjLpTozaPW6aT2rhPm3LSyjXZ5AbWH");
 
 #[program]
 pub mod anchor_escrow {
